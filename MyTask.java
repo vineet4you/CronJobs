406d83a602dc81c3ee0fad6bbe155760b2fda074
Sharma <vineet.e.sharma@accenture.com>
2021-04-11 22:57:43 +0530

    second commit

---
+++ MyTask.java
@@ -0,0 +1,14 @@
+package com.project.cron;
+
+import java.util.Date;
+import java.util.TimerTask;
+
+public class MyTask extends TimerTask  {
+
+	@Override
+	public void run() {
+	System.out.println(new Date());
+		
+	}
+
+}
